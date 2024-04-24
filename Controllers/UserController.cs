708f18f44636e91083cb4f1dcba57286fea32577
Tshiamo Justice Mothokho <s225258358@mandela.ac.za>
2024-04-24 19:16:45 +0200

    Completes UserRepository

---
+++ Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DataLayer.Models.Domain;
+using DataLayer.Models.Repository;
+
+namespace Learnsite.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserRepository _userRepository;
+
+        public UserController(UserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<User> userList = await _userRepository.GetAllUsersAsync();
+            return View(userList);
+        }
+    }
+}
---
+++ Learnsite Stored Procedures.sql
@@ -0,0 +1,59 @@
+﻿--use Learnsite;
+
+--User Table
+
+CREATE PROCEDURE spGetAllUsers
+AS
+BEGIN
+	SELECT UserID, FirstName, LastName, Email, Role
+	FROM tblUser
+	WHERE Status = 'Active';
+END
+Go;
+
+CREATE PROCEDURE spGetUserById
+	@UserID int
+AS
+BEGIN
+	SELECT FirstName, LastName, Email, Role
+	FROM tblUser
+	WHERE UserID = @UserID;
+END
+Go;
+
+CREATE PROCEDURE spInsertUser
+	@FirstName NVARCHAR(50),
+	@LastName NVARCHAR(50),
+	@Email NVARCHAR(255),
+	@Password NVARCHAR(255),
+	@Role NVARCHAR(10),
+	@Status NVARCHAR(11)
+AS
+BEGIN
+	INSERT INTO tblUser(FirstName, LastName, Email, Password, Role, Status)
+	VALUES(@FirstName, @LastName, @Email, @Password, @Role, @Status);
+END
+Go;
+
+CREATE PROCEDURE spUpdateUser
+	@UserID INT, 
+	@FirstName NVARCHAR(50),
+	@LastName NVARCHAR(50),
+	@Password NVARCHAR(255)
+AS
+BEGIN
+	UPDATE tblUser
+	SET FirstName = @FirstName, LastName = @LastName, Password = @Password
+	WHERE UserID = @UserID;
+END
+Go;
+
+CREATE PROCEDURE spDeleteUser
+	@UserID int
+AS
+BEGIN
+	UPDATE tblUser
+	SET Status = 'Not Active'
+	WHERE UserID = @UserID;
+END
+Go;
